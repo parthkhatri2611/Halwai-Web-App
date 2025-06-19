0a26b925835a3e72797e4e671a9b369e6daf482b
parthkhatri2611 <parthkhatri2611@gmail.com>
2025-06-19 18:11:00 +0530

    server.js link to netlify

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors({
-  origin: ['https://shivshakticatering.netlify.app/'],
+  origin: [ 'https://shivshakticatering.netlify.app/','https://shivshakticatering.netlify.app/'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
