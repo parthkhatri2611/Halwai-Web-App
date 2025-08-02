5c1f65a8862d0af9a96d7c99e9e4f27ccf928c22
parthkhatri2611 <parthkhatri2611@gmail.com>
2025-08-02 18:23:00 +0530

    monetag removed

--- client/src/index.js
+++ client/src/index.js
@@ -14,15 +14,3 @@ root.render(
 );
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((reg) => {
-        console.log('Service worker registered:', reg);
-      })
-      .catch((err) => {
-        console.error('Service worker registration failed:', err);
-      });
-  });
-}
