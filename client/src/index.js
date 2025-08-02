e7ac15de1500a5cf2448b6d159301520dc0f4be1
parthkhatri2611 <parthkhatri2611@gmail.com>
2025-08-02 18:19:59 +0530

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,3 +12,17 @@ root.render(
     </CartProvider>
   </React.StrictMode>
 );
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => {
+        console.log('Service worker registered:', reg);
+      })
+      .catch((err) => {
+        console.error('Service worker registration failed:', err);
+      });
+  });
+}
